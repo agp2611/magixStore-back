882233e5ac41d187bdce1bc54fe226e1b0ef7fc9
Arthur Gomes <arthurgomees03@gmail.com>
2026-03-17 20:22:41 -0300

    Model da entidade User

---
+++ src/main/java/finalproject/poo/User.java
@@ -0,0 +1,35 @@
+package finalproject.poo;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "user_type")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public abstract class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @JsonIgnore
+    private String password;
+
+    public boolean isAdmin() {};
+}
