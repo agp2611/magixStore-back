c991a8e7b6448063085affe3012f1df31579e052
Arthur Gomes <arthurgomees03@gmail.com>
2026-03-18 09:11:34 -0300

    Service da entidade Product

---
+++ src/main/java/finalproject/poo/service/ProductService.java
@@ -0,0 +1,46 @@
+package finalproject.poo.service;
+
+import finalproject.poo.exception.ProductNotFoundException;
+import finalproject.poo.model.Product;
+import finalproject.poo.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    public final ProductRepository productRepository;
+
+    public Product create(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> findAll(){
+            return productRepository.findAll();
+    }
+
+    public Product findById(Long id){
+        return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException());
+    }
+
+    public Product update(Long id ,Product updateProduct){
+        Product product = findById(id);
+
+        product.setName(updateProduct.getName());
+        product.setDescription(updateProduct.getDescription());
+        product.setStock(updateProduct.getStock());
+        product.setImageUrl(updateProduct.getImageUrl());
+        product.setPrice(updateProduct.getPrice());
+
+        return productRepository.save(product);
+    }
+
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+}
