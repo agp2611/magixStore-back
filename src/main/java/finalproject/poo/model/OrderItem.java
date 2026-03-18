ba62716fabf53c004e110c798d21ab9caa3346a6
Arthur Gomes <arthurgomees03@gmail.com>
2026-03-17 23:30:18 -0300

    Model da entidade OrderItem

---
+++ src/main/java/finalproject/poo/model/OrderItem.java
@@ -0,0 +1,31 @@
+package finalproject.poo.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    private Order order;
+
+    @ManyToOne
+    private Product product;
+
+    private Integer quantity;
+
+    private BigDecimal price;
+}
