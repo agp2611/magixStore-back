928a062528aeb5434393a79859174aaabc1811bc
Arthur Gomes <arthurgomees03@gmail.com>
2026-03-17 22:03:19 -0300

    Model da entidade Product

---
+++ src/main/java/finalproject/poo/model/Product.java
@@ -0,0 +1,35 @@
+package finalproject.poo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = true, length = 150)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = true)
+    private String imageUrl;
+}
