f59dd19546363c5c951c5e115a639c9cbe8d183b
Arthur Gomes <arthurgomees03@gmail.com>
2026-03-17 22:24:12 -0300

    Model da entidade Order

---
+++ src/main/java/finalproject/poo/model/Order.java
@@ -0,0 +1,33 @@
+package finalproject.poo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> items = new ArrayList<>();
+
+    private LocalDateTime orderDate;
+
+    private BigDecimal total;
+}
