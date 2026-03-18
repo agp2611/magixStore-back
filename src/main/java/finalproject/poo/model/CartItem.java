82f3e1516c7dcfd8a62830ab95d1ccdcd974ad9b
Arthur Gomes <arthurgomees03@gmail.com>
2026-03-17 22:13:31 -0300

    Model da entidade CartItem

---
+++ src/main/java/finalproject/poo/model/CartItem.java
@@ -0,0 +1,26 @@
+package finalproject.poo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Cart cart;
+
+    @ManyToOne
+    private Product product;
+
+    private Integer quantity;
+}
