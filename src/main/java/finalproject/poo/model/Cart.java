4738fc5298134cb53f2d768ab36483fe7a4cc557
Arthur Gomes <arthurgomees03@gmail.com>
2026-03-17 22:09:16 -0300

    Model da entidade Cart

---
+++ src/main/java/finalproject/poo/model/Cart.java
@@ -0,0 +1,28 @@
+package finalproject.poo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Cart")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<CartItem> items = new ArrayList<>();
+}
