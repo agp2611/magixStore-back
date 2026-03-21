fd3ac3612b14b15ea7ec2155858d86ed4d18139f
Arthur Gomes <arthurgomees03@gmail.com>
2026-03-21 08:30:08 -0300

    Order not found exception

---
+++ src/main/java/finalproject/poo/exception/OrderNotFoundException.java
@@ -0,0 +1,7 @@
+package finalproject.poo.exception;
+
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException() {
+        super("Order not found");
+    }
+}
